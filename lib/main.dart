4d6bf45aab32d5206bdac2581e897f514bb39a1a
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-12 15:34:18 +0600

    Initialize Firebase Core before starting NurVerse

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // lib/main.dart
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -33,6 +34,12 @@ import 'screens/more_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ============================================================
+  // FIREBASE
+  // ============================================================
+
+  await Firebase.initializeApp();
 
   // ============================================================
   // DATE LOCALIZATION
