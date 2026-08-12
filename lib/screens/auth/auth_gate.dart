7c91a979ed37fcd630621e7f8e2dfbc20e464e63
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-12 21:50:40 +0600

    Make authentication optional instead of blocking app startup

--- lib/screens/auth/auth_gate.dart
+++ lib/screens/auth/auth_gate.dart
@@ -1,45 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../../services/auth_service.dart';
 import '../main_navigation_screen.dart';
-import 'google_login_screen.dart';
 
-class AuthGate extends StatefulWidget {
+/// NurVerse is usable without an account.
+/// Authentication is an optional action opened from Home/Settings.
+class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
-  State<AuthGate> createState() => _AuthGateState();
-}
-
-class _AuthGateState extends State<AuthGate> {
-  bool _continueAsGuest = false;
-
-  @override
   Widget build(BuildContext context) {
-    if (_continueAsGuest) {
-      return const MainNavigationScreen();
-    }
-
-    return StreamBuilder<User?>(
-      stream: AuthService.instance.authStateChanges,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        if (snapshot.hasData) {
-          return const MainNavigationScreen();
-        }
-
-        return GoogleLoginScreen(
-          onContinueWithoutAccount: () {
-            setState(() => _continueAsGuest = true);
-          },
-        );
-      },
-    );
+    return const MainNavigationScreen();
   }
 }
