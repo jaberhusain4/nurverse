76252422ce152b8bc68c56cc52cd1ee1b75a12e0
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-12 16:30:55 +0600

    Fix duplicated Android Gradle build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
-}
-
 allprojects {
     repositories {
         google()
@@ -20,31 +16,6 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
 subprojects {
     project.evaluationDependsOn(":app")
 }
