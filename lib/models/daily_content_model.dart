92620805018ec2e7b6090cd8e66e5d2df75fefe9
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-14 23:01:32 +0600

    Add English fields to daily content model

--- lib/models/daily_content_model.dart
+++ lib/models/daily_content_model.dart
@@ -4,18 +4,19 @@ class DailyContentModel {
   final DailyContentType type;
 
   final String title;
-
   final String arabic;
-
   final String bangla;
-
+  final String english;
   final String reference;
+  final String englishReference;
 
   const DailyContentModel({
     required this.type,
     required this.title,
     required this.arabic,
     required this.bangla,
+    required this.english,
     required this.reference,
+    required this.englishReference,
   });
 }
