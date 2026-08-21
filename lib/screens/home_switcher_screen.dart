d21e3c66b1e5539267e8ad705ecadb57496b2607
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-21 22:15:48 +0600

    feat(home): switch home layout from persistent setting

---
+++ lib/screens/home_switcher_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../services/home_mode_service.dart';
+import 'home_screen.dart';
+import 'simple_home_screen.dart';
+
+class HomeSwitcherScreen extends StatefulWidget {
+  const HomeSwitcherScreen({super.key, this.onNavigateTab});
+
+  final Function(int)? onNavigateTab;
+
+  @override
+  State<HomeSwitcherScreen> createState() => _HomeSwitcherScreenState();
+}
+
+class _HomeSwitcherScreenState extends State<HomeSwitcherScreen> {
+  final HomeModeService _service = HomeModeService.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _service.load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _service,
+      builder: (context, _) {
+        if (!_service.isLoaded) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (_service.isSimple) {
+          return SimpleHomeScreen(onNavigateTab: widget.onNavigateTab);
+        }
+
+        return HomeScreen(onNavigateTab: widget.onNavigateTab);
+      },
+    );
+  }
+}
