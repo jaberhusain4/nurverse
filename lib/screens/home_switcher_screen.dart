6f7db194ed3a1bcd372e5469c3b8f9ea99720bcf
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-22 21:35:20 +0600

    feat: activate isolated Muslim Day style simple home v3

--- lib/screens/home_switcher_screen.dart
+++ lib/screens/home_switcher_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../services/home_mode_service.dart';
 import 'home_screen.dart';
-import 'simple_home_screen_muslim_day_v2.dart';
+import 'simple_home_screen_muslim_day_v3.dart';
 
 class HomeSwitcherScreen extends StatefulWidget {
   const HomeSwitcherScreen({super.key, this.onNavigateTab});
@@ -34,7 +34,7 @@ class _HomeSwitcherScreenState extends State<HomeSwitcherScreen> {
         }
 
         if (_service.isSimple) {
-          return SimpleHomeScreenMuslimDayV2(
+          return SimpleHomeScreenMuslimDayV3(
             onNavigateTab: widget.onNavigateTab,
           );
         }
