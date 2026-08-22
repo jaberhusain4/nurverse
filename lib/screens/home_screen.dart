79358a27d44763f4485402eb0206d039158fc93d
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-22 16:47:59 +0600

    feat(home): activate premium V11 hero

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
-import 'simple_home_screen_v6.dart';
+import 'simple_home_screen_v11.dart';
 
-class HomeScreen extends SimpleHomeScreenV6 {
+class HomeScreen extends SimpleHomeScreenV11 {
   const HomeScreen({super.key, super.onNavigateTab});
 }
