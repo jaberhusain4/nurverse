51f817c32425484cb35f8a8440ade320579e072c
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-08-25 12:49:45 +0600

    fix: repair localization guard regex syntax

--- tool/check_localization_hardcodes.dart
+++ tool/check_localization_hardcodes.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 final _bangla = RegExp(r'[\u0980-\u09FF]');
-final _directEnglishBranch = RegExp(r'\b(?:isEnglish|isBangla|isArabic|languageCode|language)\s*==?\s*[\'\"](?:en|bn|ar)[\'\"]');
+final _directEnglishBranch = RegExp(r"\b(?:isEnglish|isBangla|isArabic|languageCode|language)\s*==?\s*['\"](?:en|bn|ar)['\"]");
 final _ternaryLanguage = RegExp(r'\b(?:isEnglish|isBangla|isArabic)\s*\?');
 final _textLiteral = RegExp(r'\b(?:Text|Tooltip|SnackBar|AlertDialog|SimpleDialog|showDialog|showModalBottomSheet)\s*\(');
 
