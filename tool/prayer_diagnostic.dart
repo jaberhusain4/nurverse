63303fbc0ec24b2e77adfb88485352efffec3ace
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-09-19 18:44:48 +0600

    chore: fix diagnostic coordinates declaration

--- tool/prayer_diagnostic.dart
+++ tool/prayer_diagnostic.dart
@@ -1,7 +1,7 @@
 import 'package:adhan/adhan.dart';
 
 void main() {
-  const coordinates = Coordinates(23.8486, 90.2500);
+  final coordinates = Coordinates(23.8486, 90.2500);
   final date = DateTime(2026, 9, 19);
   final params = CalculationMethod.karachi.getParameters()..madhab = Madhab.hanafi;
   final times = PrayerTimes(coordinates, DateComponents.from(date), params);
