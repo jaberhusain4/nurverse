9de33df87a1d3b7f81d54d5ba6a12ff41aee72ce
Jaber Husain <113585594+jaberhusain4@users.noreply.github.com>
2026-09-19 12:19:08 +0600

    fix: import ValueNotifier foundation types

--- lib/controllers/prayer_controller.dart
+++ lib/controllers/prayer_controller.dart
@@ -1,5 +1,6 @@
 // lib/controllers/prayer_controller.dart
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:adhan/adhan.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
